19c1da5fba73b1c71afd3b70ca71163415bf6950
YS <yogendrasingh890@gmail.com>
2017-12-11 01:59:41 +0530

    Initial commit

---
+++ app/src/main/java/app/ys/weatherping_2/Intro.java
@@ -0,0 +1,73 @@
+package app.ys.weatherping_2;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by YS on 10-12-2017.
+ */
+
+public class Intro extends AppCompatActivity implements LocationListener {
+
+    LocationManager locationManager;
+    public Double Latitude;
+    public Double Longitude;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.intro);
+
+        Button getLocationBtn = (Button)findViewById(R.id.getLocationBtn);
+
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+
+        }
+                getLocation();
+              }
+
+    void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, this);
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Latitude=location.getLatitude();
+        Longitude=location.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(Intro.this, "Please Enable Location and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+}
